128d5267fc068f01d87f2bf94981f988021fba29
Sigmu92 <maxwellw92@gmail.com>
2020-07-27 16:50:54 -0400

    added save and load functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,4 @@
-
+require 'json'
 require 'pry'
 
 class Solution
@@ -56,35 +56,50 @@ class Game
     @guessed = guessed
     @display_word = gen_display
     @win = false
-  
+    @to_save = false
   end
 
   def run
-    puts "Welcome to hangman!"
-    binding.pry
+    
     board.display(@misses, display_word, @guessed)
-
     until misses.zero? || @win == true
-      find_input
+      @to_save = find_input
+      if @to_save
+        save_game
+        break
+      end
       if solution.check_guess?(@guess)
         index = solution.fit_letters(@guess)
         update_display(index, guess)
       else
         @misses -= 1
       end
-      
       if display_word == solution.solution
         @win = true
       end
       board.display(misses, display_word, @guessed)
     end
     ending
+    
   end
 
   # PRIVATE
 
+  def save_game
+    temp_hash = {
+      "solution" => solution.solution,
+      "misses" => "#{@misses}",
+      "guessed" => @guessed
+    }
+    File.open("save.json", "w") do |f|
+      f.write(JSON.pretty_generate(temp_hash))
+    end
+  end
+
   def ending
-    if @misses == 0
+    if @to_save == true
+      puts 'Your game has been saved!'
+    elsif @misses == 0
       puts "You lose! The word was #{solution.solution.join}!"
     else
       puts "You won! Good job!"
@@ -96,14 +111,16 @@ class Game
   end
 
   def find_input
-    puts "Enter your guess: "
+    puts "Enter your guess(type save to save game): "
     input = gets.chomp.downcase
+    return true if input=='save'
     until check_valid?(input)
       puts "Invalid guess, guess again: "
       input = gets.chomp.downcase
     end
     @guess = input
     @guessed.push(input)
+    false
   end
 
   def check_valid?(input)
@@ -126,7 +143,38 @@ class Game
   end
 
 end
-board = Board.new
-solution = Solution.new
-game = Game.new(solution,board)
+def load_game
+  board = Board.new
+  file = File.open "save.json"
+  data = JSON.load file
+  solution = data["solution"]
+  misses = data["misses"].to_i
+  guessed = data["guessed"]
+  solution = Solution.new(solution)
+  Game.new(solution,board,misses,guessed)
+end
+
+def new_game
+  board = Board.new
+  solution = Solution.new
+  Game.new(solution, board)
+end
+def start_game
+  
+  if File.exist?('save.json')
+    puts 'Would you like to load your saved game (y/n)?'
+    if gets.chomp.downcase == 'y'
+      load_game
+    else
+      new_game
+    end
+  else
+    new_game
+  end
+end
+
+
+puts 'Welcome to hangman!'
+game = start_game
 game.run
+
